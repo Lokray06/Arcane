b16e17522605e7071920d334ca6b74f481ed545b
Lokray06 <jpgp14s06@gmail.com>
2025-02-15 17:22:21 +0100

    Fully Working 3d and rendering

--- src/engine/Texture.java
+++ src/engine/Texture.java
@@ -15,25 +15,24 @@ import java.nio.IntBuffer;
 import static org.lwjgl.system.MemoryStack.stackPush;
 
 public class Texture {
-
     public enum Type {
-        ALBEDO, NORMAL, ROUGHNESS
+        ALBEDO, NORMAL, ROUGHNESS, METALLIC, AO
     }
-
+    
     private int textureID = 0; // 0 means not yet initialized
     private String path;
     private Type type;
     private Vector4i rgba;
     private boolean loaded = false; // Flag to track if the texture has been loaded
-
+    
     public Texture(String path) {
         this.path = path;
     }
-
+    
     public Texture(Type type) {
         this.type = type;
     }
-
+    
     public Texture(Vector4i rgba)
     {
         this.rgba = rgba;
@@ -42,7 +41,7 @@ public class Texture {
     {
         this.rgba = new Vector4i(rgb.x, rgb.y, rgb.z, 255);
     }
-
+    
     private void createTexture(Vector4i rgba)
     {
         int r = rgba.x;
@@ -75,61 +74,63 @@ public class Texture {
     {
         createTexture(new Vector4i(rgb.x, rgb.y, rgb.z, 255));
     }
-
-
+    
+    
     private void loadTexture(String path)
     {
         try (var stack = stackPush()) {
             IntBuffer width = stack.mallocInt(1);
             IntBuffer height = stack.mallocInt(1);
             IntBuffer channels = stack.mallocInt(1);
-
+            
             STBImage.stbi_set_flip_vertically_on_load(true);
             ByteBuffer image = STBImage.stbi_load(path, width, height, channels, 4);
-
+            
             if (image == null) {
                 throw new RuntimeException("Failed to load texture: " + path);
             }
-
+            
             textureID = GL11.glGenTextures();
             GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureID);
-
+            
             GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
             GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
             GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL11.GL_REPEAT);
             GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL11.GL_REPEAT);
-
+            
             GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, width.get(), height.get(), 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, image);
             GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
-
+            
             STBImage.stbi_image_free(image);
             GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
-
+            
             loaded = true;
         }
     }
-
+    
     private void createDefaultTexture(Type type) {
         System.out.println("Creating default texture: " + type);
         textureID = GL11.glGenTextures();
         GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureID);
         ByteBuffer pixel = BufferUtils.createByteBuffer(4);
-
+        
         switch (type) {
             case ALBEDO -> pixel.put((byte) 255).put((byte) 255).put((byte) 255).put((byte) 255);
             case NORMAL -> pixel.put((byte) 128).put((byte) 128).put((byte) 255).put((byte) 255);
             case ROUGHNESS -> pixel.put((byte) 128).put((byte) 128).put((byte) 128).put((byte) 255);
+            case METALLIC -> pixel.put((byte) 0).put((byte) 0).put((byte) 0).put((byte) 255); // non-metal by default
+            case AO -> pixel.put((byte) 255).put((byte) 255).put((byte) 255).put((byte) 255); // full ambient occlusion
         }
         pixel.flip();
-
+        
         GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, 1, 1, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, pixel);
         GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
         GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
         GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
-
+        
         loaded = true;
     }
-
+    
     public void ensureLoaded() {
         if (!loaded) {
             if (path != null) {
@@ -142,15 +143,15 @@ public class Texture {
             }
         }
     }
-
-
+    
+    
     public void bind(int unit)
     {
         ensureLoaded(); // Ensure the texture is loaded before binding
         GL13.glActiveTexture(GL13.GL_TEXTURE0 + unit);
         GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureID);
     }
-
+    
     public void delete() {
         if (textureID != 0) {
             GL11.glDeleteTextures(textureID);
@@ -158,12 +159,12 @@ public class Texture {
             loaded = false;
         }
     }
-
+    
     public int getID() {
         ensureLoaded(); // Ensure texture is loaded before retrieving ID
         return textureID;
     }
-
+    
     @Override
     public String toString() {
         if(path != null)
